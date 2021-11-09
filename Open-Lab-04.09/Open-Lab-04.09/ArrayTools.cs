ba87bd81b02514dbebee2d57a2534a8f43b66d8c
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-11-09 21:47:43 +0100

    removes similar string from string array

---
+++ Open-Lab-04.09/Open-Lab-04.09/ArrayTools.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Open_Lab_04._09
+{
+    public class ArrayTools
+    {
+        public string[] RemoveDups(string[] strings)
+        {
+            var done = new List<string> {};
+            foreach (string s in strings)
+            {
+                if (done.Contains(s))
+                {
+                }
+                else
+                {
+                    done.Add(s);
+                }
+            }
+            return done.ToArray();
+
+
+        }
+    }
+}
